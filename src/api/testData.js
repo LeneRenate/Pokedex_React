7a1214d4c9855d8edd1fd5e764f4d1589a2268cb
LeneRenate <Lene.renate.rosseboe@gmail.com>
2025-12-11 13:16:39 +0100

    added images and static testdata

--- src/api/testData.js
+++ src/api/testData.js
@@ -1,15 +1,54 @@
-import { fetchPokemon } from "./axiosPokeAPI";
+import bulbasaurImg from "../assets/250px-0001Bulbasaur.png";
+import ivysaurImg from "../assets/250px-0002Ivysaur.png";
+import venusaurImg from "../assets/250px-0003Venusaur.png";
+import charmanderImg from "../assets/250px-0004Charmander.png";
+import charmeleonImg from "../assets/250px-0005Charmeleon.png";
+import charizardImg from "../assets/250px-0006Charizard.png";
 
-export async function testPokemons(n) {
-  const ids = [...Array(n)].map((_, i) => i + 1);
-  console.log(ids);
+const testPokemon = {
+  id: 1,
+  name: "Bulbasaur",
+  image: { bulbasaurImg },
+  types: ["grass", "poison"],
+};
 
-  const pokemonArray = await Promise.all(ids.map((id) => fetchPokemon(id)));
+const testArray = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    image: { bulbasaurImg },
+    types: ["grass", "poison"],
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    image: { ivysaurImg },
+    types: ["grass", "poison"],
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    image: { venusaurImg },
+    types: ["grass", "poison"],
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    image: charmanderImg,
+    types: ["fire"],
+  },
+  {
+    id: 5,
+    name: "Charmeleon",
+    image: { charmeleonImg },
+    types: ["fire"],
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    image: { charizardImg },
+    types: ["fire", "flying"],
+  },
+];
 
-  console.log(pokemonArray);
-  console.log(typeof pokemonArray);
-
-  return pokemonArray;
-}
-
-testPokemons(3);
+export { testPokemon, testArray };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,39 @@
-import { useEffect, useState } from "react";
-import { fetchPokemon, IMG_URL } from "../api/axiosPokeAPI";
+import { testPokemon } from "../api/testData";
 import PokemonCard from "../components/PokemonCard";
 import styles from "../styles/HomePage.module.css";
 
 export function HomePage() {
-  const [apiResponse, setApiResponse] = useState(null);
+  console.log(testPokemon);
 
-  useEffect(() => {
-    fetchPokemon(3).then(setApiResponse);
-  }, [apiResponse]);
+  const p = testPokemon;
+  console.log(p.name);
+  console.log(p.image);
+
+  const typeString = p.types.join(", ");
+  console.log(typeString);
 
   return (
     <>
       <section className={` ${styles.pokeDisplay}`}>
         <h2 className={` ${styles.homeTitle}`}>Some pokemons</h2>
-        <ul className={`flex flex-row g-8`}>
-          {apiResponse.results.map((p) => (
+        <PokemonCard
+          id={p.id}
+          image={p.image}
+          title={p.name}
+          types={typeString}
+        />
+        {/* <ul className={`flex flex-row g-8`}>
+          {testArray.forEach((p) => (
             <li key={p.id} className={` ${styles.cardWrapper}`}>
               <PokemonCard
                 id={p.id}
-                image={`${IMG_URL}${p.id}.png`}
+                image={p.image}
                 title={p.name}
-                types={p.types.map((t) => t.type.name)}
+                types={typeString}
               />
             </li>
           ))}
-        </ul>
+        </ul> */}
       </section>
     </>
   );
