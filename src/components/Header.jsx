24b772024e7e1b2ef9f23bd12f09e57acabfcc00
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-01-05 10:05:51 +0100

    Applied type-styles to types

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,45 @@
+import { useEffect, useRef } from "react";
 import styles from "../styles/Header.module.css";
 import pokedexImg from "../assets/pokedex.png";
+import { pokemonTypes } from "../api/typesList";
+import applyTypeStyles from "../utils/typeStyles";
 
 export default function Header() {
+  const types = pokemonTypes;
+  const typeRefs = useRef([]);
+
+  useEffect(() => {
+    typeRefs.current.forEach((element, index) => {
+      if (element) {
+        applyTypeStyles(element, [types[index]]);
+      }
+    });
+  }, [types]);
+
   return (
     <header
-      className={`w-full h-28 sticky top-0 p-6 flex flex-row ${styles.wholeHeader}`}
+      className={`w-full h-32 sticky top-0 px-8 py-4 flex flex-row justify-start items-center ${styles.wholeHeader}`}
     >
-      <div className={`h-4/5 ${styles.imgWrapper}`}>
+      <div className={`h-9/10 ${styles.imgWrapper}`}>
         <img
           src={pokedexImg}
           alt="pokedex from the TV series"
           className={`h-full ${styles.headerImg}`}
         />
       </div>
-      <h1 className={styles.pageTitle}>PokeDex</h1>
-      <ul className={`grid grid-rows-3 gap-3 ${styles.typesKeyList}`}>
-        <li className={`${styles.typeKeys}`}></li>
+      <h1 className={`p-4 pl-8 ${styles.pageTitle}`}>PokeDex</h1>
+      <ul className={`grid grid-cols-6 gap-3 ${styles.typesKeyList}`}>
+        {types.map((type, index) => (
+          <li
+            key={type}
+            ref={(el) => (typeRefs.current[index] = el)}
+            className={`${styles.typeKeys}`}
+          >
+            {type}
+          </li>
+        ))}
       </ul>
     </header>
   );
 }
+// bg - [--card - bg];
