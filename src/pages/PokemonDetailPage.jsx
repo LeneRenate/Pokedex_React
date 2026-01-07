75068e278590265d67f1f3ffde55ce4c16602ded
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-01-07 10:55:48 +0100

    Added pages/routes

---
+++ src/pages/PokemonDetailPage.jsx
@@ -0,0 +1,28 @@
+// Make into a page!
+
+export default function PokemonDetailPage() {
+  return (
+    <>
+      {/* img + name + idnumber */}
+      <article className={`flex flex-col`}>
+        <p>id</p>
+        <h3>Pokemon name</h3>
+        <img src="" alt="" />
+      </article>
+
+      {/* Primary info */}
+      <article className={`flex flex-col`}>
+        <p>Type</p>
+        <p>Category</p>
+        <p>Height</p>
+        <p>Weight</p>
+        <p>Abilities</p>
+      </article>
+
+      {/* Attack + info */}
+      <article></article>
+
+      {/* Evolving */}
+    </>
+  );
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { HomePage } from "../pages/HomePage";
+import PokemonDetailPage from "../pages/PokemonDetailPage";
+import SearchResultsPage from "../pages/SearchResultsPage";
 
 export const router = createBrowserRouter([
   {
     path: "Pokedex_React",
     element: <App />,
     errorElement: <h1>An error occured</h1>,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "pokemon/:name", element: <PokemonDetailPage /> },
+      { path: "search", element: <SearchResultsPage /> },
+      { path: "*", element: <h1>404 not found</h1> },
+    ],
   },
 ]);
